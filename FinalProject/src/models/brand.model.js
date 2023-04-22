b681b7fd5a06c1fa6719570db71a66c36b04aba3
LuanVo <vothanhluan19102001@gmail.com>
2023-04-22 23:51:05 +0700

    refactor

--- FinalProject/src/models/brand.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Brand = new Schema({
-    name: String
-})
-
-module.exports = mongoose.model('Brand', Brand);
--- FinalProject/src/models/category.model.js
+++
@@ -1,8 +0,0 @@
-const mongoose = require('mongoose');
-const Schema = mongoose.Schema;
-
-const Category = new Schema({
-    category: String
-})
-
-module.exports = mongoose.model('Category', Category);
